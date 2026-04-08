7ce3d3663908edfe1529c8581e9c18c69b9a662b
Pratyaksh Sharma <pratyaksh.shm@gmail.com>
2026-04-08 13:33:09 +0530

    improved ui

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,24 @@ const cors = require("cors");
 
 const app = express();
 
-// ================= MIDDLEWARE =================
 app.use(cors());
 app.use(express.json());
 
 
-// ================= ROUTES =================
 app.use("/api/assignments", require("./routes/assignments"));
 
 
-// ================= DATABASE CONNECTION =================
-
-// 🔴 REPLACE with your actual Atlas connection string
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log("DB Error:", err));
 
-// ================= TEST ROUTE =================
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 
-// ================= SERVER =================
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
